504a2f8ab100e8c550ec9bf5892a7efdfff370c7
CVaranese <Chris.Varanese@varmaco.com>
2016-03-13 16:15:10 -0700

    Driver updates, generate finish

---
+++ HumanDriver.java
@@ -0,0 +1,5 @@
+public class HumanDriver{
+   public static void main(String[] args){
+//      SudokuGame game = new SudokuGame();
+   }
+}
--- SudokuGame.java
+++ SudokuGame.java
@@ -4,6 +4,15 @@
  */
 public class SudokuGame {
    private int[][] board;
+   private static final int[][] BEGINBOARD = {{1,2,3,4,5,6,7,8,9},
+                                 {4,5,6,7,8,9,1,2,3},
+                                 {7,8,9,1,2,3,4,5,6},
+                                 {2,3,4,5,6,7,8,9,1},
+                                 {5,6,7,8,9,1,2,3,4},
+                                 {8,9,1,2,3,4,5,6,7},
+                                 {3,4,5,6,7,8,9,1,2},
+                                 {6,7,8,9,1,2,3,4,5},
+                                 {9,1,2,3,4,5,6,7,8}};
 
    public SudokuGame(int[][] b){
       board = b;
@@ -80,5 +89,88 @@ public class SudokuGame {
          System.out.println();
       }
       System.out.println("+---+---+---+");
+   }
+
+   public static int[][] generateBoard(int difficulty){
+      double chance;
+      if (difficulty == 0){
+         chance = .4;
+      } else if (difficulty == 1){
+         chance = .55;
+      } else if (difficulty == 2){
+         chance = .7;
+      } else {
+         throw new IllegalArgumentException();
+      }
+      int[][] newBoard = BEGINBOARD;
+      System.out.println("Starting loop...");
+      for(int q = 0; q < 100; q++){
+         System.out.println("Started loop...");
+         int r = (int)(Math.random()*5);
+         int sw1 = (int)(Math.random()*9);
+         int sw2 = (int)(Math.random()*9);
+         System.out.println("Checking equals....");
+         while (sw2 == sw1){
+            System.out.println(sw1 + "  " + sw2);
+            sw2 = (int)(Math.random()*9);
+         }
+         System.out.println("Checked equals...");
+         if (r == 0){
+            System.out.println("r = 0");
+            for (int i = 0; i < 9; i ++){
+               for (int j = 0; j < 9; j++){
+                  if (newBoard[i][j] == sw1){
+                     newBoard[i][j] = sw2;
+                  } else if (newBoard[i][j] == sw2){
+                     newBoard[i][j] = sw1;
+                  }
+               }
+            }
+            System.out.println("Finished r = 0");
+         }
+         if (r == 1){
+            System.out.println("r = 1");
+            int temp = 0;
+            for(int i = 0; i < 9; i++){
+               temp = newBoard[sw1][i];
+               newBoard[sw1][i] = newBoard[sw2][i];
+               newBoard[sw2][i] = temp;
+            }
+            System.out.println("Finished r = 1");
+         }
+         if (r == 2){
+            System.out.println("r = 2");
+            int temp = 0;
+            for(int i = 0; i < 9; i ++){
+               temp = newBoard[i][sw1];
+               newBoard[i][sw1] = newBoard[i][sw2];
+               newBoard[i][sw2] = temp;
+            }
+            System.out.println("Finished r = 2");
+         }
+         if (r == 3){
+            System.out.println("r = 3");
+            int temp = 0;
+            int[][] newerBoard = new int[9][9];
+            for(int i = 0; i < 9; i++){
+               for(int j = 0; j < 9; j++){
+                  newerBoard[i][j] = newBoard[8 - j][i];
+               }
+            }
+            System.out.println("Finished r = 3");
+            newBoard = newerBoard;
+         }
+      System.out.println("Finished iteration: " + q);
+      }
+      System.out.println("starting dig....");
+      for (int i = 0; i < 9; i++){
+         for (int j = 0; j < 9; j++){
+            if (Math.random() < chance){
+               newBoard[i][j] = 0;
+            }
+         }
+      }
+      System.out.println("Finished dig!");
+      return newBoard;
    }
 }
