ba167de97ef5014e9ce10f43e1f9c613c44b8f0f
Nicholas Fernandes <npfernan@csclnx04.ad.calpoly.edu>
2016-03-16 19:09:47 -0700

    Random Improvements

--- testPlethora.java
+++ testPlethora.java
@@ -10,7 +10,7 @@ public class testPlethora{
       int[][] temp2 = new int[9][9];
       double t1=0;
       double t2 =0;
-      for(int i =0; i < 10000; i++){
+      for(int i =0; i < 1000; i++){
          dif =((int) (Math.random()*3));
          difSum+=dif;
          temp = SudokuGame.generateBoard(dif);
@@ -26,9 +26,9 @@ public class testPlethora{
          dumb += (t1 - t2);
          System.out.println("Test Number: " + (i +1));
       }
-      dumb = dumb/10000;
-      smart = smart/10000;
-      difSum = difSum/10000;
+      dumb = dumb/1000;
+      smart = smart/1000;
+      difSum = difSum/1000;
       System.out.println("Average Diffuculty: " + difSum);
       System.out.println("Average time for Smart Ai: " + smart + " milliseconds");
       System.out.println("Average time for Dumb Ai: " + dumb + " milliseconds");
