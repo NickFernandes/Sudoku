f59c1f18e9fadfef3953d609c54aa88ac37a9a52
Nicholas Fernandes <npfernan@csclnx11.ad.calpoly.edu>
2016-03-16 18:38:13 -0700

    testPlethora is more plethoric

--- testPlethora.java
+++ testPlethora.java
@@ -2,33 +2,33 @@ import java.util.Arrays;
 public class testPlethora{
    public static void main(String[] args){
       System.out.println("Running 1000 tests");
-      long dumb =0;
-      long smart=0;
+      double dumb =0;
+      double smart=0;
       int dif =0;
       double difSum = 0;
       int[][] temp = new int[9][9];
       int[][] temp2 = new int[9][9];
-      long t1=0;
-      long t2 =0;
-      for(int i =0; i < 1000; i++){
+      double t1=0;
+      double t2 =0;
+      for(int i =0; i < 10000; i++){
          dif =((int) (Math.random()*3));
          difSum+=dif;
          temp = SudokuGame.generateBoard(dif);
          for(int j = 0; j < 9; j++){
             temp2[j] = Arrays.copyOf(temp[j], 9);
          }
-         t1 = System.currentTimeMillis();
+         t1 = System.nanoTime()/1000000;
          temp = SmartAi.solve(temp);
-         t2 = System.currentTimeMillis();
+         t2 = System.nanoTime()/1000000;
          smart += (t2 - t1);
          temp2 = DumbAi.solve(temp2);
-         t1 = System.currentTimeMillis();
+         t1 = System.nanoTime()/1000000;
          dumb += (t1 - t2);
          System.out.println("Test Number: " + (i +1));
       }
-      dumb = dumb/1000;
-      smart = smart/1000;
-      difSum = difSum/1000;
+      dumb = dumb/10000;
+      smart = smart/10000;
+      difSum = difSum/10000;
       System.out.println("Average Diffuculty: " + difSum);
       System.out.println("Average time for Smart Ai: " + smart + " milliseconds");
       System.out.println("Average time for Dumb Ai: " + dumb + " milliseconds");
