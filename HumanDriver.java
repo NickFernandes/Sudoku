34ad77737e5374acf855bd9e84c39360c95f859f
CVaranese <Chris.Varanese@varmaco.com>
2016-03-18 00:03:18 -0700

    duplicate code rewritten

--- HumanDriver.java
+++ HumanDriver.java
@@ -12,25 +12,18 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 public class HumanDriver{
+   private static final Scanner sc = new Scanner(System.in);
    /**
     *Plays the game.
     */
    public static void main(String[] args){
       if(args.length == 0){
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Would you like to read from a file?(y/n)");
-         String read = sc.nextLine().toLowerCase();
-         while(!(read.equals("y") || read.equals("n"))){
-            System.out.println("Please enter \"y\" or \"n\"");
-            read = sc.nextLine().toLowerCase();
-         }
+         boolean read = getUserYesOrNo("Would you like to read from a file?(y/n)\n" +
+                                       "This will read from the file \"gameBoard.txt\"");
          SudokuGame game = null;
          File file;
-            while (read.equals("y")){
-            //file
-            System.out.println("Enter the name of the file:");
-            String fileName = sc.nextLine();
-            file = new File(fileName);
+         if (read){
+            file = new File("gameBoard.txt");
             if (file.canRead()){
                int[][] gameNums = new int[9][9];
                boolean[][] gameChange = new boolean[9][9];
@@ -52,8 +45,10 @@ public class HumanDriver{
                         int boo = buff.read() - 48;
                         if (boo == 0){
                            gameChange[i][j] = false;
-                        } else{
+                        } else if (boo == 1){
                            gameChange[i][j] = true;
+                        }  else {
+                           throw new IOException();
                         }
                      }
                   }
@@ -70,107 +65,35 @@ public class HumanDriver{
                      game.setChangeable(i, j, gameChange[i][j]);
                   }
                }
-               break;
-               //read file
-               //break
             }
             else {
                System.out.println("Cannot read file.");
             }
          }
-         if (read.equals("n")){
+         if (!read){
             System.out.println("What would you like the difficulty to be?\n" +
                         "Enter \"0\", \"1\", or \"2\", with 0 being the easiest.");
-            int diff = (int)(sc.next().charAt(0) - 48);
-            while (!(diff == 1 || diff == 0 || diff == 2)){
-               System.out.println("Between 0 and 2, please:");
-               diff = (int)(sc.next().charAt(0) - 48);
-            }
+            int diff = getUserInteger(0, 2);
             game = new SudokuGame(diff);
          }
-         //game.printBoard();
+         System.out.println("Your game is now starting. You may quit out at any time, " +
+                           "and your board will be saved to the document \"gameBoard.txt\"");
          boolean solved = false;
          while (!solved){
             game.printBoard();
-            System.out.println("Enter \"q\" if you would like to quit or anything if not. Changes" +
-                        " will be saved to a file called \"gameBoard.txt\"");
-            String quit = sc.next();
-            System.out.println(quit);
-            if(quit.equals("q")){
-               File write = new File("gameBoard.txt");
-               if (write.exists()){
-                  write.delete();
-               }
-               try{
-                  write.createNewFile();
-               } catch(IOException ex){
-                  System.out.println("Cannot create new file.");
-               }
-               PrintStream strim;
-               try{
-                  strim = new PrintStream(write);
-               } catch (FileNotFoundException ex){
-                  strim = null;
-                  System.out.println("Could not create print stream");
-               }
-               if (write.canWrite()){
-                  for (int i = 0; i < 9; i ++){
-                     for (int j = 0; j < 9; j ++){
-                        if (strim != null){
-                           strim.print(game.get(i,j));
-                        }
-                     }
-                  }
-                  for (int i = 0; i < 9; i ++){
-                     for (int j = 0; j < 9; j ++){
-                        if (strim != null){
-                           if (game.getChangeable(i,j)){
-                              strim.print(1);
-                           } else{
-                              strim.print(0);
-                           }
-                        }
-                     }
-                  }
-               }
-               write.setReadOnly();
-               break;
-               //write file
-               //break
-            }
             System.out.println("Enter the row of the element you wish to change");
-            int row = (int)(sc.next().charAt(0) - 48);
-            while (row < 0 || row > 8){
-               System.out.println("Between 0 and 8, please:");
-               row = (int)(sc.next().charAt(0) - 48);
-            }
+            int row = getUserInteger(0, 8);
             System.out.println("Enter the column of the element you wish to change");
-            int column = (int)(sc.next().charAt(0) - 48);
-            while (column < 0 || column > 8){
-               System.out.println("Between 0 and 8, please:");
-               column = (int)(sc.next().charAt(0) - 48);
-            }
+            int column = getUserInteger(0, 8);
             while (!game.getChangeable(column, row)){
                System.out.println("That entry is not changeable. Please Enter new values.");
                System.out.println("Row:");
-               row = (int)(sc.next().charAt(0) - 48);
-               while (row < 0 || row > 8){
-                  System.out.println("Between 0 and 8, please:");
-                  row = (int)(sc.next().charAt(0) - 48);
-               }
+               row = getUserInteger(0, 8);
                System.out.println("Column:");
-               column = (int)(sc.next().charAt(0) - 48);
-               while (column < 0 || column > 8){
-                  System.out.println("Between 0 and 8, please:");
-                  column = (int)(sc.next().charAt(0) - 48);
-               }
+               column = getUserInteger(0, 8);
             }
-            System.out.println("Enter the number you wish to enter (1 - 9)");
-            int replace = (int)(sc.next().charAt(0) - 48);
-            while (replace < 1 || replace > 9){
-               System.out.println("Between 0 and 8, please:");
-               replace = (int)(sc.next().charAt(0) - 48);
-            }
+            System.out.println("Enter the number you wish to enter (0 - 9)");
+            int replace = getUserInteger(0, 9);
             game.set(column, row, replace);
             boolean finished = true;
             for (int i = 0; i < 9; i ++){
@@ -186,11 +109,48 @@ public class HumanDriver{
             }
             if (finished){
                System.out.println("Your board is filled!");
-               solved = game.checkBoardSolved();//check it
+               solved = game.checkBoardSolved();
                if (!solved){
                   System.out.println("But, it's not solved yet!");
                }
             }
+            File write = new File("gameBoard.txt");
+            if (write.exists()){
+               write.delete();
+            }
+            try{
+               write.createNewFile();
+            } catch(IOException ex){
+               System.out.println("Cannot create new file.");
+            }
+            PrintStream strim;
+            try{
+               strim = new PrintStream(write);
+            } catch (FileNotFoundException ex){
+               strim = null;
+               System.out.println("Could not create print stream");
+            }
+            if (write.canWrite()){
+               for (int i = 0; i < 9; i ++){
+                  for (int j = 0; j < 9; j ++){
+                     if (strim != null){
+                        strim.print(game.get(i,j));
+                     }
+                  }
+               }
+               for (int i = 0; i < 9; i ++){
+                  for (int j = 0; j < 9; j ++){
+                     if (strim != null){
+                        if (game.getChangeable(i,j)){
+                           strim.print(1);
+                        } else{
+                           strim.print(0);
+                        }
+                     }
+                  }
+               }
+            }
+            write.setReadOnly();
          }
          if (solved){
             System.out.println("Congratulations! You did it!");
@@ -199,5 +159,46 @@ public class HumanDriver{
       else{
          System.out.println("Please do not run with arguments");
       }
+   }
+
+   private static int getUserInteger(int min, int max){
+      System.out.println("Enter a number between " + min + " and " + max + ":");
+      int next = 0;
+      boolean acceptedInput = false;
+      while (!acceptedInput){
+         try{
+            next = Integer.parseInt(sc.nextLine());
+            if (next >= min && next <= max){
+               acceptedInput = true;
+            } else {
+               System.out.println("Between " + min + " and " + max + " please.");
+            }
+         } catch(NumberFormatException ex){
+            System.out.println("Between " + min + " and " + max + " please.");
+         }
+      }
+      return next;
+   }
+   private static boolean getUserYesOrNo(String str){
+      System.out.println(str);
+      boolean next = false;
+      boolean acceptedInput = false;
+      while (!acceptedInput){
+         try{
+            String response = sc.nextLine();
+            if (response.equals("y")){
+               next = true;
+               acceptedInput = true;
+            } else if (response.equals("n")){
+               next = false;
+               acceptedInput = true;
+            } else{
+               System.out.println("Please enter \"y\" or \"n\"");
+            }
+         } catch(NumberFormatException ex){
+            System.out.println("Please enter \"y\" or \"n\"");
+         }
+      }
+      return next;
    }
 }
